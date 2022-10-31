855b35723bb14440ac2b305131187fdec23fe72f
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-10-31 14:08:22 +0900

    feat(main, timer): 기본적인 틀 생성

--- app/src/main/java/com/example/object_oriented_pj_10/MainActivity.kt
+++ app/src/main/java/com/example/object_oriented_pj_10/MainActivity.kt
@@ -7,6 +7,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-// test
     }
+    override fun onStart() {
+        super.onStart()
+
+        initNumberPicker()
+        numberPickerListener()
+    }
+    private fun initNumberPicker() {
+        val data1 : Array<String> Array(100) {
+            i -> i.toString()
+        }
+        numberPicker.minValue = 0
+        numberPicker.maxValue = data1.size -1
+        numberPicker.wrapSelectorWheel = false
+        numberPicker.displayValues = data1
+    }
+
+    private fun numberPickerListener() {
+        numberPicker.setOnValueChangedListener {picker, oldVal, newVal ->
+            Log.d("test", "oldVal : ${oldVal}, newVal : ${newVal}")
+            Log.d("test", "picker.displayedValues ${picker.displayedValues[picker.value]}")
+
+        }
+    }
+
 }
