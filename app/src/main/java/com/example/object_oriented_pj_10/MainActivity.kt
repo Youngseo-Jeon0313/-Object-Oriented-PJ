440521560e43154f971d4d881c734f8142c36aee
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-01 23:58:05 +0900

    feat: 버튼, numberPicker 전체적인 css 구성

--- app/src/main/java/com/example/object_oriented_pj_10/MainActivity.kt
+++ app/src/main/java/com/example/object_oriented_pj_10/MainActivity.kt
@@ -2,34 +2,13 @@ package com.example.object_oriented_pj_10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    override fun onStart() {
-        super.onStart()
-
-        initNumberPicker()
-        numberPickerListener()
-    }
-    private fun initNumberPicker() {
-        val data1 : Array<String> Array(100) {
-            i -> i.toString()
-        }
-        numberPicker.minValue = 0
-        numberPicker.maxValue = data1.size -1
-        numberPicker.wrapSelectorWheel = false
-        numberPicker.displayValues = data1
-    }
-
-    private fun numberPickerListener() {
-        numberPicker.setOnValueChangedListener {picker, oldVal, newVal ->
-            Log.d("test", "oldVal : ${oldVal}, newVal : ${newVal}")
-            Log.d("test", "picker.displayedValues ${picker.displayedValues[picker.value]}")
-
-        }
     }
 
 }
